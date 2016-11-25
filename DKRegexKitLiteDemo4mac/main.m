95c5226b6a5faf50e10331743d77dbcc4c96c9b9
kevin <929118967@qq.com>
2016-11-25 16:12:35 +0800

    init

--- DKRegexKitLiteDemo4mac/main.m
+++ DKRegexKitLiteDemo4mac/main.m
@@ -7,11 +7,36 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "RegexKitLite.h"
 
-int main(int argc, const char * argv[]) {
+// 创业天使-xxx 120101_超清.mp4 --> 120101-创业天使-xxx.mp4
+int main(int argc, const char * argv[])
+{
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSFileManager *mgr = [NSFileManager defaultManager];
+        NSString *dir = @"/Users/apple/Desktop/videos";
+        NSArray *subpaths = [mgr subpathsAtPath:dir];
+        for (NSString *subpath in subpaths) {
+            if (![subpath hasSuffix:@"mp4"]) continue;
+            // 获得全路径
+            NSString *fullSubpath = [dir stringByAppendingPathComponent:subpath];
+            // 获得文件名
+            NSString *filename = [subpath.lastPathComponent stringByDeletingPathExtension];
+            
+            // 根据文件名获取对应的前缀和后缀
+            NSString *prefix = [filename stringByMatching:@"(\\d{6})_.清" capture:YES];
+            NSString *suffix = [filename stringByMatching:@"(.+) \\d{6}_.清" capture:YES];
+            NSString *newFilename = [NSString stringWithFormat:@"%@-%@", prefix, suffix];
+            
+            // 生成新的全路径（替换）
+            NSString *newFullSubpath = [fullSubpath stringByReplacingOccurrencesOfString:filename withString:newFilename];
+            //            NSData *data = [NSData dataWithContentsOfFile:fullSubpath];
+            //            [data writeToFile:newFullSubpath atomically:YES];
+            //            [mgr removeItemAtPath:fullSubpath error:nil];
+            
+            // 剪切\移动
+            [mgr moveItemAtPath:fullSubpath toPath:newFullSubpath error:nil];
+        }
     }
     return 0;
 }
